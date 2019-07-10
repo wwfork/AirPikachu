4037b1b8f70115fd65e1074bf0d326d9157a7302
csbatista88 <christian.dev88@gmail.com>
2019-07-09 22:32:27 -0300

    atualizacao02
    upgrade

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :fullname, presence: true, length: { maximum: 500 }
+
 end
